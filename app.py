82e7c47c0e5eeed67c7b07a9d1e969736ee4cd9e
IDGS-805-22001761 <78649@alumnos.utleon.edu.mx>
2025-02-27 18:51:59 -0600

    layout

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, g
+from flask_wtf import CSRFProtect
+from config import DevelopmentConfig
+from models import db, Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect()
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
 
 @app.route("/")
 @app.route("/index")
@@ -9,3 +18,8 @@ def index():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from sqlalchemy import create_engine
+import urllib 
+
+class Config(object):
+    SECRET_KEY = 'mysecretkey'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'msql+pymysql://root:password@localhost/bdidgs805'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
